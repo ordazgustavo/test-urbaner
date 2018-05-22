a853db5e551cd02e7128534a9c91935554bd468c
Gustavo Ordaz <ordazsgustavo@gmail.com>
2018-05-21 23:10:58 -0500

    Netlify deploy
    - .env variables
    - Redireccion

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -5,7 +5,16 @@ import 'firebase/storage'
 
 import { config } from './firebaseConfig'
 
-firebase.initializeApp(config)
+const envConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSEGING_SENDER_ID
+}
+
+firebase.initializeApp(config || envConfig)
 
 export const database = firebase.database()
 export const auth = firebase.auth()
